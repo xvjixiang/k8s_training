bde2352ee6c69f19b1ef5b0253ace8e129c9e3ca
i516296 <jixiang.xv@sap.com>
2021-11-18 17:27:41 +0800

    make manager to watch all namespaces

--- operator/src/memcached-operator/main.go
+++ operator/src/memcached-operator/main.go
@@ -72,6 +72,7 @@ func main() {
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "87a7d51f.example.com",
+		Namespace:              "",
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
